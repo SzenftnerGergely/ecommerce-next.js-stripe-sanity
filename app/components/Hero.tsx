1400ffbd4e14891f0d7e5041a0fdb5f7d3c16489
SzenftnerGergely <gergely.szenftner@gmail.com>
2023-11-20 17:51:56 +0100

    hero links added

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Image from 'next/image'
 import { client, urlFor } from '../lib/sanity'
+import Link from 'next/link'
 
 async function getData() {
     const query = "*[_type == 'heroImage'][0]"
@@ -16,7 +17,7 @@ const Hero = async () => {
     return (
         <section className='mx-auto max-w-2xl px-4 sm:pb-6 lg:max-w-7xl lg:px-8'>
             <div className='mb-8 flex flex-wrap justify-between md:mb-16'>
-                 <div className='mb-6 flex w-full flex-col justify-center sm:mb-12 lg:mb-0 lg:w-1/3 lg:pb-24 lg:pt-48'>
+                <div className='mb-6 flex w-full flex-col justify-center sm:mb-12 lg:mb-0 lg:w-1/3 lg:pb-24 lg:pt-48'>
                     <h1 className='mb-4 tex-4xl font-bold text-black sm:text-5xl md:mb-8 md:text-6xl'>
                         Top Fashion for a top price!
                     </h1>
@@ -24,12 +25,12 @@ const Hero = async () => {
                         We sell only the most exclusive and high quality products for you.
                         We are the best so come and shop with us.
                     </p>
-                 </div>
+                </div>
 
-                 <div className='mb-12 flex w-full md:mb-16 lg:w-2/3'>
+                <div className='mb-12 flex w-full md:mb-16 lg:w-2/3'>
                     <div className='relative left-12 top-12 z-10 -ml-12 overflow-hidden rounded-lg bg-gray-100 shadow-lg md:left-16 md:top-16 lg:mgl-0'>
-                        <Image 
-                            src={urlFor(data.image1).url()} 
+                        <Image
+                            src={urlFor(data.image1).url()}
                             alt="Great Photo"
                             className='h-full w-full object-cover object-center'
                             priority
@@ -40,7 +41,7 @@ const Hero = async () => {
 
                     <div className='overflow-hidden rounded-lg bg-gray-100 shadow-lg'>
                         <Image
-                            src={urlFor(data.image2).url()} 
+                            src={urlFor(data.image2).url()}
                             alt='Great Photo'
                             className='h-full w-full object-cover object-center'
                             priority
@@ -48,8 +49,23 @@ const Hero = async () => {
                             height={500}
                         />
                     </div>
-                 </div>
+                </div>
             </div>
+
+            <div className='flex flex-col items-center justify-between gap-8 md:flex-row'>
+                <div className='flex h-12 w-64 divide-x overflow-hidden rounded-lg border'>
+                    <Link href="/Men" className='flex w-1/3 items-center justify-center text-gray-500 transition duration-100 hover:bg-gray-100 active:bg-gray-200'>
+                        Men
+                    </Link>               
+                    <Link href="/Women" className='flex w-1/3 items-center justify-center text-gray-500 transition duration-100 hover:bg-gray-100 active:bg-gray-200'>
+                        Women
+                    </Link>
+                    <Link href="/Teens" className='flex w-1/3 items-center justify-center text-gray-500 transition duration-100 hover:bg-gray-100 active:bg-gray-200'>
+                        Teens
+                    </Link>
+                </div>
+            </div>
+             
         </section>
     )
 }
